51c429bd31e2386f2504aca65088b2243511407e
nyavo <johanesanyavo@gmail.com>
2020-09-17 15:20:40 +0000

    Verification si les utilisateurs sont inscrits

--- script_utilisateur/check_user.sh
+++ script_utilisateur/check_user.sh
@@ -1,39 +1,56 @@
 #!/bin/bash
 
-arrVar=( 0 )
+
+if [ $# -ne 1 ]
+then
+	echo "Specifier le fichier xls en argument positionel comme suit"
+	echo "$0 <fichier.xls>"
+	exit 99
+fi
+
+
+
+USERS=( 0 )
+
+
+#Prendre la liste des users et mettre dans l'array USERS
 
 index=0
 
-
 while IFS=: read -r username _ _ _ _ dir _
 do
-    arrVar[$index]=$username
-    echo ${arrVar[$index]}
+    USERS[$index]=$username
     index=$(($index+1))
 done <<<$(grep /home /etc/passwd | sort -t: -k6 )
 
 
 
 
-INPUT=test.csv
+
+INPUT=$1
+
 OLDIFS=$IFS
 IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 while read nom prenom username
 do
-        if ["$username" == "username"]
+        if [[ "${username:1:-1}"  = "username" ]] || [ -z  "${username:1:-1}" ]
 	then
 		continue
 	fi
 
-	if [$username in $arrVar]
+	#Verification si le username est déjà inscrit 
+
+	if [[ " ${USERS[@]} " =~ " ${username:1:-1} " ]];
 	then
-		echo "yes"
+		echo "${username:1:-1} est déjà inscrit"
 	else
-		echo $username
+		echo "${username:1:-1} n'est pas inscrit"
 	fi
 
-done < $INPUT
+done <<<$(xls2csv ${INPUT})
 IFS=$OLDIFS
+
+
 
 
